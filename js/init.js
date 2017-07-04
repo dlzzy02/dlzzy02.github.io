5eec9560d442c06e485bd6d23d033c58a64c418f
dlzzy02 <dlzzy02@naver.com>
2017-07-04 18:06:48 +0900

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,13 @@
+jQuery(function(){
+    var slider = $('.bxslider').bxSlider({
+    mode: 'horizontal',
+    });
+    var slider = $('.comment').bxSlider({
+    mode: 'vertical',
+    minSlides: 5,
+    maxSlides: 5,
+    moveSlides: 1,
+    auto:true,
+
+    });
+})
